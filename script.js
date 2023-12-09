68e1c733be19dcbf09e06671ff55bdf661aaa3ac
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-12-09 22:45:26 +0000

    Formatted divs into a 16x16 grid

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 function createGrid() {
     const containerDiv = document.querySelector(".container");
 
-    for (let i=0; i < 15; i++) {
+    for (let i=0; i < 256; i++) {
         let childDiv = document.createElement('div');
         childDiv.className = "square";
         containerDiv.append(childDiv);
     }
+    
 }
 
 createGrid();
