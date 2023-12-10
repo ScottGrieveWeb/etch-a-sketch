6777eeba0858a798d38b08351216757a21cd89b7
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-12-10 12:41:20 +0000

    Added a reset button
    -This reset button completely clears the grid
    -It also prompts the user to enter the size of the grid. The prompt
    works, but the program doesn't do anything with the user's prompt yet.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createGrid() {
         let childDiv = document.createElement('div');
         childDiv.className = "square";
         childDiv.classList.add(`div${i}`)
-        childDiv.addEventListener("mouseover", randomColour);
+        childDiv.addEventListener("mouseover", colourDrawing);
         containerDiv.append(childDiv);
         
     }
@@ -16,8 +16,36 @@ function createGrid() {
 
 createGrid();
 
-function randomColour(e) {
+function colourDrawing(e) {
 
     e.target.style.backgroundColor = 'black';
    
+}
+
+function clearGrid() {
+
+    let grid = document.querySelectorAll('.square');
+
+    console.log(grid);
+
+    for ( i = 0; i < grid.length; i++) {
+        grid[i].style.backgroundColor = "white";
+        };
+
+    // grid.style.backgroundColor = 'white';
+   
+}
+
+document.getElementById("reset").onclick = buttonFunc;
+
+function buttonFunc() {
+    clearGrid();
+
+    let userChoice = prompt("Enter how many squares you would like per side (max = 100): ");
+
+    if (userChoice > 100) {
+        userChoice = prompt("That's too many squares! The max is 100:")
+    } 
+
+    console.log(userChoice);
 }
